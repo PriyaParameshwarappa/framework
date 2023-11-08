b8aa9cc2ccc2b8fec40a98ac964cfea9ea5bfc7a
Priya <Priya@DESKTOP-SGK5HUK>
2023-11-08 11:38:09 +0530

    changes done

--- src/test/java/page/DashBoardPage.java
+++ src/test/java/page/DashBoardPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class DashBoardPage
+public class DashBoardPage 
 {
 	@FindBy(xpath="//h6[text()='Dashboard']")
 	private WebElement dashboard;
